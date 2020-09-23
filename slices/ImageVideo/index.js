0a65997d512620741d598ac17241fe5921ad0574
HaydenCai-Arkade <internship@arkade.com.au>
2020-09-23 23:16:05 +1000

    replace the homepage component with slice-machine

--- slices/ImageVideo/index.js
+++ slices/ImageVideo/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { array, shape } from "prop-types";
-import { RichText } from "prismic-reactjs";
+import React from 'react';
+import { array, shape } from 'prop-types';
+import { RichText } from 'prismic-reactjs';
 
 const MySlice = ({ slice }) => {
   return (
@@ -10,24 +10,46 @@ const MySlice = ({ slice }) => {
           {slice.primary.title ? (
             <RichText render={slice.primary.title} />
           ) : null}
-          {slice.primary.content ? (
-            <RichText render={slice.primary.content} />
-          ) : null}
-          {slice.primary.picture ? (
-            <img src={slice.primary.picture.url} />
-          ) : null}
-          {slice.primary.video ? (
-            <div
-              dangerouslySetInnerHTML={{
-                __html: slice.primary.video.html,
-              }}
-            />
-          ) : null}
+          <div className="picture">
+            {slice.primary.picture ? (
+              <img src={slice.primary.picture.url} className="picture" />
+            ) : null}
+          </div>
+          <div>
+            {slice.primary.content ? (
+              <RichText render={slice.primary.content} />
+            ) : null}
+          </div>
+          <div className="video">
+            {slice.primary.video ? (
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: slice.primary.video.html,
+                }}
+              />
+            ) : null}
+          </div>
         </div>
       </div>
       <style>{`
+      .image-video-content{
+        max-width:1100px;
+        margin:0 auto;
+        text-algin:center;
+        padding-top:50px;
+      }
 
+      .picture{
+        width:1100px;
+        margin-bottom:30px;
+      }
 
+      iframe{
+        width:1100px;
+        height:400px;
+        margin-top:50px;
+   
+      }
 
     `}</style>
     </div>
