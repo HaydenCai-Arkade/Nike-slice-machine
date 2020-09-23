1abe4e831c1eca18b382eafd8790aa74ebac0bc6
HaydenCai-Arkade <internship@arkade.com.au>
2020-09-23 22:28:38 +1000

    added imagesGallery slice and adjusted storybook

--- slices/ImagesGallerySlice/index.js
+++ slices/ImagesGallerySlice/index.js
@@ -1,23 +1,59 @@
-import React from "react";
-import { array, shape } from "prop-types";
-import { RichText } from "prismic-reactjs";
+import React from 'react';
+import { array, shape } from 'prop-types';
+import { RichText } from 'prismic-reactjs';
 
-const section = {
-  maxWidth: "600px",
-  margin: "4em auto",
-  textAlign: "center",
+const MySlice = ({ slice }) => {
+  return (
+    <div className="image-gallery">
+      <div className="image-gallery-content">
+        <div>
+          <div className="images-content">
+            {slice.primary.title ? (
+              <RichText render={slice.primary.title} />
+            ) : null}
+            {slice.primary.content ? (
+              <RichText render={slice.primary.content} />
+            ) : null}
+          </div>
+          <div className="images">
+            {slice.items
+              ? slice.items.map((item, index) => {
+                  return (
+                    <img
+                      key={index}
+                      src={item.image.url}
+                      className="gallery-image"
+                    />
+                  );
+                })
+              : null}
+          </div>
+        </div>
+      </div>
+      <style>{`
+
+      .image-gallery{
+        display:flex;
+        justify-content:center;
+      }
+      .images-content{
+        text-align:center;
+        padding: 0 100px;
+      }
+      .images{
+        width:1000px;
+        margin:0 auto;
+      }
+
+      .gallery-image{
+        height:250px;
+        width:230px;
+        margin-right:20px;
+      }
+    `}</style>
+    </div>
+  );
 };
-
-const h2 = {
-  color: "#8592e0",
-};
-
-const MySlice = ({ slice }) => (
-  <section style={section}>
-    <h2 style={h2}>Template slice, update me!</h2>
-    {slice.primary.title ? <RichText render={slice.primary.title} /> : <p></p>}
-  </section>
-);
 
 MySlice.propTypes = {
   slice: shape({
--- slices/index.js
+++ slices/index.js
@@ -1,8 +1,5 @@
-export { default as IntroBanner } from "./IntroBanner";
-export { default as FeatureSlice } from "./FeatureSlice";
-export { default as PriceSlice } from "./PriceSlice";
-export { default as ImagesSlice } from "./ImagesSlice";
-export { default as ColumnImageSlice } from "./ColumnImageSlice";
-export { default as ImageVideo } from "./ImageVideo";
-
-export { default as ImagesGallerySlice } from "./ImagesGallerySlice";
+export { default as IntroBanner } from './IntroBanner';
+export { default as PriceSlice } from './PriceSlice';
+export { default as ColumnImageSlice } from './ColumnImageSlice';
+export { default as ImageVideo } from './ImageVideo';
+export { default as ImagesGallerySlice } from './ImagesGallerySlice';
